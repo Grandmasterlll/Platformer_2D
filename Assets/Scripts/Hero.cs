135cf4023a0eacc88fb761af3906b8778ebb42b0
Grandmasterlll <derek14022002@gmail.com>
2022-12-22 01:41:48 +0300

    master

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum States
+{
+    idle,
+    run,
+    jump 
+}
+
 public class Hero : MonoBehaviour
 {
     [SerializeField] private float speed = 3f;
@@ -11,11 +18,20 @@ public class Hero : MonoBehaviour
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private Animator animator;
+
+    private States States
+    {
+        get { return (States)animator.GetInteger("state"); }
+        set { animator.SetInteger("state",(int)value); }
+    }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
     }
 
     private void FixedUpdate()
@@ -25,10 +41,16 @@ public class Hero : MonoBehaviour
 
     private void Update()
     {
+        if(isGrounded == true)
+        {
+            States = States.idle;
+        }
+
         if (Input.GetButton("Horizontal"))
         {
             Run();
         }
+
         //isGrounded==true &&
         if (isGrounded==true && Input.GetButton("Jump"))
         {
@@ -38,6 +60,11 @@ public class Hero : MonoBehaviour
 
     private void Run()
     {
+        if (isGrounded == true)
+        {
+            States = States.run;
+        }
+
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime );
         sprite.flipX = dir.x < 0.0f;
@@ -51,7 +78,12 @@ public class Hero : MonoBehaviour
     private void CheckGround()
     {
         
-        Collider2D[] colliders = Physics2D.OverlapCircleAll( transform.position, 0.9f);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll( transform.position, 0.3f);
         isGrounded = colliders.Length > 1;
+
+        if (isGrounded == true)
+        {
+            States = States.jump;
+        }
     }
 }
